192b51a3eb96ba698103a0f15b6377145cbd004d
BuilderMARK <73698747+BuilderMARK@users.noreply.github.com>
2020-12-09 11:36:49 +0100

    Bedre mappe struktur og nye fxml filer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,22 +14,25 @@ public class Controller {
 
 
     public void load1(javafx.event.ActionEvent actionEvent) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("kun.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("gui/kun.fxml"));
         rootpane.getChildren().setAll(pane);
     }
 
     public void loadback(ActionEvent actionEvent) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource(
+            "gui/sample.fxml"));
         rootpane.getChildren().setAll(pane);
     }
 
     public void load2(ActionEvent actionEvent) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("team.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource(
+            "gui/team.fxml"));
         rootpane.getChildren().setAll(pane);
     }
 
     public void load3(ActionEvent actionEvent) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("opgaver.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource(
+            "gui/opgaver.fxml"));
         rootpane.getChildren().setAll(pane);
     }
 
