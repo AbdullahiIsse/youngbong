6d1c8900fb6aa0982223b9d14fc67f03eaebb47d
Mark <Markhjuler@gmail.com>
2020-12-10 23:53:51 +0100

    Tabel

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -54,16 +54,18 @@ public class Controller {
     }
 
     public void intialize (URL url , ResourceBundle resourceBundle){
-        TreeTableColumn <DataObject, String> attribute1Column = new TreeTableColumn<DataObject, String>("Attribute1");
-        TreeTableColumn <DataObject, Integer> attribute2Column = new TreeTableColumn<DataObject, Integer>("Attribute2");
-        TreeTableColumn <DataObject, String> attribute3Column = new TreeTableColumn<DataObject, String>("Attribute3");
+        TreeTableColumn <DataObject, String> attribute1Column = new TableColumn<>("Attribute1");
+        TreeTableColumn <DataObject, Integer> attribute2Column = new TableColumn<>("Attribute2");
+        TreeTableColumn <DataObject, String> attribute3Column = new TableColumn<>("Attribute3");
 
-        attribute1Column.setCellValueFactory(new PropertyValueFactory("attribute1"));
-        attribute2Column.setCellValueFactory(new PropertyValueFactory("attribute2"));
-        attribute3Column.setCellValueFactory(new PropertyValueFactory("attribute3"));
+        attribute1Column.setCellValueFactory(new PropertyValueFactory<>("attribute1"));
+        attribute2Column.setCellValueFactory(new PropertyValueFactory<>("attribute2"));
+        attribute3Column.setCellValueFactory(new PropertyValueFactory<>("attribute3"));
+        dataObjectTableView.getColumns().add(attribute1Column);
+        dataObjectTableView.getColumns().add(attribute2Column);
+        dataObjectTableView.getColumns().add(attribute3Column);
 
-
-
+        dataObjectTableView.getItems().addAll(model.getAllDataObjects());
 
     }
 
