fc6dd567daaa2faa08288124cd333eb64f2c9b9e
Abdllahi isse <abdullahi12351@gmail.com>
2020-12-10 22:16:03 +0100

    test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,26 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+import javax.print.DocFlavor;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class Controller {
     @FXML private AnchorPane rootpane;
+    @FXML private TreeTableView thetable;
 
 
     public void load1(javafx.event.ActionEvent actionEvent) throws IOException {
@@ -39,6 +49,16 @@ public class Controller {
     public void musklik(MouseEvent mouseEvent) {
 
         System.exit(0);
+    }
+
+    public void intialize (URL url , ResourceBundle resourceBundle){
+        TreeTableColumn navn = new TreeTableColumn("Navn");
+        TreeTableColumn telefon = new TreeTableColumn("telefon");
+        TreeTableColumn rolle = new TreeTableColumn("rolle");
+
+        thetable.getColumns().addAll(navn,telefon,rolle);
+
+
     }
 
 
