66b18e791a91585c4e57a807b1da80b8d6dd912b
Abdllahi isse <abdullahi12351@gmail.com>
2020-12-13 01:21:58 +0100

    test123

--- src/sample/KundeController.java
+++ src/sample/KundeController.java
@@ -3,14 +3,19 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.converter.IntegerStringConverter;
 import sample.metoder.Kunde;
 
 
@@ -22,10 +27,14 @@ import java.util.ResourceBundle;
 public class KundeController implements Initializable {
     @FXML
     private AnchorPane rootpane;
+    @FXML
+    private Button add;
     @FXML private TableView<Kunde>table;
     @FXML private TableColumn<Kunde,String>Fornavn;
     @FXML private TableColumn<Kunde,String>Efternavn;
     @FXML private TableColumn<Kunde, Integer> Telefonnummer;
+
+
 
 
 
@@ -57,19 +66,60 @@ public class KundeController implements Initializable {
 
 
     public void initialize(URL location, ResourceBundle rb){
+
+        table.setEditable(true);
+
         Fornavn.setCellValueFactory(new PropertyValueFactory<Kunde,String>("Fornavn"));
+        Fornavn.setCellFactory(TextFieldTableCell.forTableColumn());
+        Fornavn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Kunde, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Kunde, String> kundeStringCellEditEvent) {
+                Kunde kunde = kundeStringCellEditEvent.getRowValue();
+                kunde.setFornavn(kundeStringCellEditEvent.getNewValue());
+            }
+        });
+
+
         Efternavn.setCellValueFactory(new PropertyValueFactory<Kunde,String>("Efternavn"));
+        Efternavn.setCellFactory(TextFieldTableCell.forTableColumn());
+        Efternavn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Kunde, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Kunde, String> kundeStringCellEditEvent) {
+                Kunde kunde = kundeStringCellEditEvent.getRowValue();
+                kunde.setEfternavn(kundeStringCellEditEvent.getNewValue());
+
+            }
+        });
+
         Telefonnummer.setCellValueFactory(new PropertyValueFactory<Kunde,Integer>("Telefonnummer"));
+        Telefonnummer.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        Telefonnummer.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Kunde, Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Kunde, Integer> kundeIntegerCellEditEvent) {
+                Kunde kunde = kundeIntegerCellEditEvent.getRowValue();
+                kunde.setTelefonnummer(kundeIntegerCellEditEvent.getNewValue());
+                list.setAll();
+            }
+        });
+
         table.setItems(list);
 
 
     }
 
 
+    public void Add(ActionEvent actionEvent) throws IOException {
 
+        AnchorPane pane = FXMLLoader.load(getClass().getResource(
+                "gui/Kundestage.fxml"));
+        rootpane.getChildren().setAll(pane);
+    }
 
+    public void remove(ActionEvent actionEvent) {
 
-
-
-
+        ObservableList<Kunde>sletOpgave, sletOpgaver;
+        sletOpgave=table.getItems();
+        sletOpgaver=table.getSelectionModel().getSelectedItems();
+        sletOpgaver.forEach(sletOpgave::remove);
+    }
 }
--- src/sample/forsideController.java
+++ src/sample/forsideController.java
@@ -3,14 +3,18 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import sample.metoder.Kunde;
 
 import java.io.IOException;
 
 public class forsideController {
     @FXML
     private AnchorPane rootpane;
+    @FXML
+
 
     public void load1(javafx.event.ActionEvent actionEvent) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("gui/kun.fxml"));
@@ -34,6 +38,6 @@ public class forsideController {
 
         System.exit(0);
     }
-
-
 }
+
+
