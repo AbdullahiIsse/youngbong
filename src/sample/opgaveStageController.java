3036562c6690862a9b964155c54285c2c402c153
Abdllahi isse <abdullahi12351@gmail.com>
2020-12-14 01:07:22 +0100

    test123

--- src/sample/opgaveStageController.java
+++ src/sample/opgaveStageController.java
@@ -1,5 +1,13 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import sample.metoder.Opgaver;
+
 public class opgaveStageController {
+    @FXML
+    private TableView<Opgaver> tableViewOpgaver;
+
 
 }
